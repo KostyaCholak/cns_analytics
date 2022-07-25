7fd4db7bc1276e00234e8ce128c60283ee6ec741
Kostya Cholak <kostya.cholak@anvileight.com>
2022-07-25 21:57:33 +0300

    Update storage structure

--- cns_analytics/storage.py
+++ cns_analytics/storage.py
@@ -1,5 +1,5 @@
+import logging
 import os
-import threading
 
 import feather
 import pandas as pd
@@ -13,12 +13,13 @@ from cns_analytics.entities import MDType
 from paramiko.py3compat import decodebytes
 
 
-# paramiko.util.get_logger('paramiko').setLevel(logging.WARN)
+paramiko.util.get_logger('paramiko').setLevel(logging.WARN)
 load_dotenv('.env')
 
 class Storage:
+    local_folder = os.getenv("STORAGE_FOLDER", ".cache/")
+    remote_folder = "/upload/cns_analytics/"
     _storage = None
-    _lock = threading.Lock()
 
     @classmethod
     def get(cls):
@@ -31,7 +32,7 @@ class Storage:
 
         if "STORAGE_HOST_KEY" in os.environ:
             key = paramiko.Ed25519Key(data=decodebytes(os.environ["STORAGE_HOST_KEY"].encode()))
-            ssh.get_host_keys().add('[109.87.62.99]:2222', 'ssh-ed25519', key)
+            ssh.get_host_keys().add(os.environ["STORAGE_HOST_NAME"], 'ssh-ed25519', key)
 
         ssh.connect(
             hostname=os.environ['STORAGE_HOST'],
@@ -45,7 +46,7 @@ class Storage:
         self.ssh = ssh
 
     def _exists_remote(self, key):
-        remote_path = os.path.join("/upload/cns_analytics/", key)
+        remote_path = os.path.join(self.remote_folder, key)
         try:
             self.sftp.stat(remote_path)
             return True
@@ -53,30 +54,46 @@ class Storage:
             return False
 
     def _exists_locally(self, key):
-        local_path = os.path.join(".cache/", key)
+        local_path = os.path.join(self.local_folder, key)
         return os.path.exists(local_path)
 
     def _upload(self, key):
-        local_path = os.path.join(".cache/", key)
-        remote_path = os.path.join("/upload/cns_analytics/", key)
+        local_path = os.path.join(self.local_folder, key)
+        remote_path = os.path.join(self.remote_folder, key)
+        folders, filename = os.path.split(key)
+        base = self.remote_folder
+
+        for folder in folders.split('/'):
+            base = os.path.join(base, folder)
+            try:
+                self.sftp.mkdir(base)
+            except OSError:
+                continue
+
         self.sftp.put(local_path, remote_path)
 
     def _download(self, key):
-        remote_path = os.path.join("/upload/cns_analytics/", key)
-        local_path = os.path.join(".cache/", key)
+        remote_path = os.path.join(self.remote_folder, key)
+        local_path = os.path.join(self.local_folder, key)
+
+        folders, filename = os.path.split(key)
+        os.makedirs(os.path.join(folders), exist_ok=True)
+
         self.sftp.get(remote_path, local_path)
 
     def _deserialize(self, key):
-        local_path = os.path.join(".cache/", key)
+        local_path = os.path.join(self.local_folder, key)
         return feather.read_dataframe(local_path)
 
     def _serialize(self, key, data: pd.DataFrame):
-        local_path = os.path.join(".cache/", key)
+        local_path = os.path.join(self.local_folder, key)
+        folders, filename = os.path.split(local_path)
+        os.makedirs(os.path.join(folders), exist_ok=True)
         return feather.write_dataframe(data, local_path)
 
     @staticmethod
     def _get_key(symbol: Symbol, md_type: MDType) -> str:
-        return f"{symbol.name}&{symbol.exchange.name}&{md_type.name}"
+        return f"{symbol.exchange.name}/{md_type.name}/{symbol.name}"
 
     @classmethod
     def load_data(cls, symbol: Symbol, md_type: MDType) -> pd.DataFrame:
