3dcc8efeb35965b0146763cbaa054a7bfeaa4c3a
Kostya Cholak <kostya.cholak@gmail.com>
2022-09-14 12:59:27 +0300

    Move fred import

--- cns_analytics/market_data/__init__.py
+++ cns_analytics/market_data/__init__.py
@@ -1,7 +1,6 @@
 from datetime import timedelta
 from cns_analytics.market_data.barchart_daily import BarchartDailyLoader
 from cns_analytics.market_data.barchart_intraday import BarchartIntradayLoader
-from cns_analytics.market_data.fred_loader import FredLoader
 from cns_analytics.entities import Symbol, Exchange, MDType, Resolution
 
 async def download_data(symbol_name, exchange, duration=None):
@@ -24,5 +23,6 @@ async def download_data(symbol_name, exchange, duration=None):
             symbol.loader_external_name = f"{symbol_name}*0" if fut else symbol_name
             await loader.fetch(symbol, md_type=MDType.OHLC, duration=duration, resolution=Resolution.m1)
     elif exchange is Exchange.Fred:
+        from cns_analytics.market_data.fred_loader import FredLoader
         await FredLoader().load(symbol, MDType.OHLC)
 
